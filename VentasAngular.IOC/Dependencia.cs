ac4b6f79c4df96721082a543485c164a3f5f0f9f
sserrano <sserrano@dgsc.go.cr>
2024-03-22 10:43:52 -0600

    Agregando el Inyectador de Dependencias

---
+++ VentasAngular.IOC/Dependencia.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VentasAngular.DAL.DBContext;
+
+namespace VentasAngular.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencias(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options => 
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
